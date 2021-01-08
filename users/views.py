20d4caf0d7781110160c472ba2af6f464b17b0d5
tuseef123 <tossefashraf.292791@gmail.com>
2021-01-08 23:14:14 +0500

    ✨ login logout

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
 def register(request):
@@ -8,8 +9,11 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}')
-            return redirect('/')
+            messages.success(request,f'Account has been created you can now login!')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request,'users/register.html',{'form':form})
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
